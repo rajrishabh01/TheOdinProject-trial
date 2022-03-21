720ba628550392cb1cd57e10ac9c61dbb0010c5c
Rishabh-mac <raj.rishabh01@gmail.com>
2022-03-21 12:53:15 +0530

    Commit for assignment css and some JS assigments

---
+++ odin-recipes/javascript/fundamentals/fundamental-javascript-3.js
@@ -0,0 +1,54 @@
+
+//To convert input string to Number use + or Number() or parseInt
+let num = +prompt("enter number", 5);
+
+function add7(input){
+    let result = input + 7;
+    return result;
+}
+
+alert(add7(num))
+
+
+// Arrow functions
+let sumArrow = a => a + 7;
+alert(sumArrow(4));
+
+let diffArrow = (a, b) => a - b;
+alert(diffArrow(5, 7))
+
+let sayHi = () => alert(`Hello!`)
+sayHi();
+
+
+//complex arrow function
+let question = prompt("Are you an employed", "yes");
+
+let ask = (question === "yes") ?
+() => alert('I have some bad news') :
+() => alert('Makes sense');
+
+ask();
+
+//practice with confirm
+//let confirmButton = prompt("is this working")
+
+let confirmArrow = (confirm("is this working?") == true) ?
+() => alert('yes it is') :
+() => alert('Nope')
+
+confirmArrow();
+
+//Capitalize First letter
+let capitalizeFirst = (string1) => {
+    return string1.charAt(0).toUpperCase() + string1.slice(1).toLowerCase();
+}
+
+alert(capitalizeFirst("hello WORLD"));
+
+//Pick iut the last letter
+let pickLastLetter = (string2) => {
+    return string2.charAt(string2.length -1);
+}
+
+alert(pickLastLetter("HDFC Bank"));
